9f1f4e25f939776f8bcf1c04b18df24945065b56
Nick <neverate@gmail.com>
2020-05-05 17:04:19 +0300

    upd merge-ranges

--- arrays-and-strings/merge-ranges.py
+++ arrays-and-strings/merge-ranges.py
@@ -15,19 +15,18 @@ def merge_ranges(meetings):
           if (meeting[0] < result[0] and meeting[1] > result[1]):
                 results[idx] = (meeting[0], meeting[1])
 
-          if (meeting[0] < result[0] and meeting[1] < result[0]
-          or meeting[0] > result[1] and meeting[1] > result[1]):
-              flag = True
-          else:
+          if not ((meeting[0] < result[0] and meeting[1] < result[0]
+          or meeting[0] > result[1] and meeting[1] > result[1])):
               flag = False
+
       if flag == True:
           results.append(meeting)      
 
     return results
 
 
-input = [(0, 1), (3, 5), (4, 8), (10, 12), (10, 12), (9, 10)]
-print(merge_ranges(input) == [(0, 1), (3, 8), (9, 12)])
+input = [(0, 1), (3, 5), (4, 8), (10, 12), (10, 12), (9, 10), (17, 46)]
+print(merge_ranges(input) == [(0, 1), (3, 8), (9, 12), (17, 46)])
 print(merge_ranges([(1, 2), (2, 3)]) == [(1, 3)])
 print(merge_ranges([(2, 3), (1, 5)]) == merge_ranges([(1, 5), (2, 3)]) == [(1, 5)])
 print(merge_ranges([(1, 10), (2, 6), (3, 5), (7, 9)]) == [(1, 10)])
