278549374775a3b10a51a3c2b74acc862823ad68
Nick <neverate@gmail.com>
2020-05-11 11:03:25 +0300

    #2 count-words upd solution fixed some bugs

--- hashing-hash-tables/c1_count_words.py
+++ hashing-hash-tables/c1_count_words.py
@@ -8,23 +8,31 @@ def count_words(inputStr):
     result = {}
     wordIsOver = False
     for idx, char in enumerate(inputStr):
-        if char in delimeters and idx < len(inputStr) - 1:
+        if char in delimeters:
             wordIsOver = True
         else:
-            if wordIsOver or idx == len(inputStr) - 1:
-                lwrCasedWord = word.lower()
-                if lwrCasedWord in result:
-                    result[lwrCasedWord] += 1
-                else:
-                    result[lwrCasedWord] = 1
+            if wordIsOver:
+                insert_word(result, word)
                 wordIsOver = False                
                 word = ''
-
             word += char
     
-    print(result)
+    if word != '':
+        insert_word(result, word)
+    
+    return result
+
+def insert_word(result, word):
+    lwrCasedWord = word.lower()
+    if lwrCasedWord in result:
+        result[lwrCasedWord] += 1
+    else:
+        result[lwrCasedWord] = 1
 
 
-count_words('After beating the eggs, Dana read the next step:')
-count_words("We came, we saw, we conquered...then we ate Bill's (Mille-Feuille) cake.")
-count_words('The bill came to five dollars.')
+
+print(count_words('After beating the eggs, Dana read the next step:'))
+print(count_words("We came, we saw, we conquered...then we ate Bill's (Mille-Feuille) cake."))
+print(count_words('The bill came to five dollars.'))
+print(count_words('A good day to die'))
+print(count_words('How about a beer?'))
